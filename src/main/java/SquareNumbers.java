192cc6082664f87a5aaaa0eb3c844a8792761f91
Szentirmai-Schon Adam <adamsan3@gmail.com>
2022-06-10 14:28:23 +0200

    implement if number is negative -> output zero

--- src/main/java/SquareNumbers.java
+++ src/main/java/SquareNumbers.java
@@ -3,13 +3,19 @@
  * <p>
  * Write a program, that prints out the squares:
  * - of the first 10 positive numbers.
+ * - if a number is negative, print out zero instead if the square
  */
 public class SquareNumbers {
     public static void main(String[] args) {
-        int[] numbers = range(5, 10);
+        int[] numbers = range(-3, 4);
         for (int i = 0; i < numbers.length; i++) {
-            int x = numbers[i] * numbers[i];
-            System.out.println(x);
+            if (numbers[i] < 0) {
+                int x = 0;
+                System.out.println(numbers[i] + " -> " + x);
+            } else {
+                int x = numbers[i] * numbers[i];
+                System.out.println(numbers[i] + " -> " + x);
+            }
         }
     }
 
